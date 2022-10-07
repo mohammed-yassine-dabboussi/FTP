dd6730e6914cda35d367371e54afb0c1eb9f5814
mohammed-yassine-dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-10-07 13:20:03 +0200

    [mise à jour job 9]

--- Job09/job09.sh
+++ Job09/job09.sh
@@ -3,3 +3,24 @@
 # Donner les droits Sudo 
 If 
 sudo usermod -a -G sudo 'user'
+
+#!/bin/bash
+cd ~/Bureau/shell-exe/job9
+cat home/yassine/Bureau/shell-exe/job9/Shell_Userlist.csv | while read varligne
+do
+	password=`echo $varligne |cut -d ',' -f4`
+	username=`echo $varligne | cut -d ',' -f2`
+	username=`echo ${username,,}`
+	role=`echo $varligne |cut -d ',' -f5` 
+	echo $role
+	if [ ${role:0:5} = "Admin" ]
+	then
+		echo "creation de l'utilisateur : $username"
+		sudo adduser -m -$username -p $password
+		echo "changement de rôle de : $username"
+		sudo usermod -aG sudo $username
+	else
+		echo "creation de l'utilisateur : $username"
+		sudo adduser -m $username -p $password
+	fi		
+done < <(tail -n +2 Shell_Userlist.csv)
